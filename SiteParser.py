4351db4e9fd1c86758b86420b10f63b9547567a3
kreodont <dimasanov@gmail.com>
2018-12-01 16:03:30 +0300

    Fixed couple issues

--- SiteParser.py
+++ SiteParser.py
@@ -22,17 +22,17 @@ def spell_nice_print(s: Spell) -> str:
 
 Spell.__repr__ = spell_nice_print
 
-attributes_translations_dict = {'уровень'           : 'level',
-                                'школа'             : 'school',
+attributes_translations_dict = {'уровень': 'level',
+                                'школа': 'school',
                                 'время накладывания': 'cast_time',
-                                'дистанция'         : 'range',
-                                'компоненты'        : 'components',
-                                'длительность'      : 'duration',
-                                'классы'            : 'classes',
-                                'источник'          : 'source',
+                                'дистанция': 'range',
+                                'компоненты': 'components',
+                                'длительность': 'duration',
+                                'классы': 'classes',
+                                'источник': 'source',
                                 'на больших уровнях': 'higher_levels',
-                                'имя'               : 'name',
-                                'описание'          : 'description',
+                                'имя': 'name',
+                                'описание': 'description',
                                 }
 
 
@@ -40,7 +40,7 @@ async def fetch_spell(*, eng_spell_name: str, session: aiohttp.client.ClientSess
         -> typing.Optional[Spell]:
     searching_url = 'http://dungeon.su/spells/'
     # searching_url = f'http://dungeon.su/spells/{eng_spell_name}'
-    async with session.get(searching_url, headers={'Accept'      : 'text/html,application/xhtml+xml,application/xml',
+    async with session.get(searching_url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml',
                                                    'Content-Type': 'text/html'},
                            params={'search': eng_spell_name}) as response:
         if debug:
@@ -49,35 +49,46 @@ async def fetch_spell(*, eng_spell_name: str, session: aiohttp.client.ClientSess
 
         response_binary = await response.read()
         html = BeautifulSoup(response_binary.decode('utf-8'), 'html.parser')
-        articles = html.find_all(name='div', attrs={'itemtype': "https://schema.org/Article"})
-        # type: typing.List[BeautifulSoup.element.Tag]
+        articles = html.find_all(name='div', attrs={
+            'itemtype': "https://schema.org/Article"})  # type: typing.List[BeautifulSoup.element.Tag]
+        if len(articles) == 0:
+            print(f'No spells with name "{eng_spell_name}" found')
+            return
+        elif len(articles) > 1:
+            names = []
+            for article in articles:
+                name_tag = article.find('a', attrs={'class': 'item-link', 'itemprop': 'url'})
+                if '(' in name_tag.text:
+                    eng_name = name_tag.text.split('(')[1].strip().replace(')', '')
+                else:
+                    eng_name = name_tag.text
+                names.append(eng_name)
+            print(f'{len(articles)} spells with name "{eng_spell_name}" found: {names}\nPlease refine your search')
 
-        if len(articles) != 1:
-            print(f'Expected to get 1 spell block, {len(articles)} found: {articles}')
             return None
 
-        spell_attributes_dict = {'level'        : SpellAttribute(ru_name='уровень', ru_value=-1,
-                                                                 en_name='level', en_value=-1),
-                                 'school'       : SpellAttribute(ru_name='школа', ru_value='нет',
-                                                                 en_name='school', en_value='na'),
-                                 'cast_time'    : SpellAttribute(ru_name='время накладывания', ru_value='нет',
-                                                                 en_name='cast_time', en_value='na'),
-                                 'duration'     : SpellAttribute(ru_name='длительность', ru_value='na',
-                                                                 en_name='duration', en_value='na'),
-                                 'range'        : SpellAttribute(ru_name='дистанция', ru_value='na',
-                                                                 en_name='range', en_value='na'),
-                                 'components'   : SpellAttribute(ru_name='компоненты', ru_value=[],
-                                                                 en_name='components', en_value=[]),
-                                 'classes'      : SpellAttribute(ru_name='классы', ru_value=[],
-                                                                 en_name='classes', en_value=[]),
-                                 'source'       : SpellAttribute(ru_name='источник', ru_value='na',
-                                                                 en_name='source', en_value='na'),
+        spell_attributes_dict = {'level': SpellAttribute(ru_name='уровень', ru_value=-1,
+                                                         en_name='level', en_value=-1),
+                                 'school': SpellAttribute(ru_name='школа', ru_value='нет',
+                                                          en_name='school', en_value='na'),
+                                 'cast_time': SpellAttribute(ru_name='время накладывания', ru_value='нет',
+                                                             en_name='cast_time', en_value='na'),
+                                 'duration': SpellAttribute(ru_name='длительность', ru_value='na',
+                                                            en_name='duration', en_value='na'),
+                                 'range': SpellAttribute(ru_name='дистанция', ru_value='na',
+                                                         en_name='range', en_value='na'),
+                                 'components': SpellAttribute(ru_name='компоненты', ru_value=[],
+                                                              en_name='components', en_value=[]),
+                                 'classes': SpellAttribute(ru_name='классы', ru_value=[],
+                                                           en_name='classes', en_value=[]),
+                                 'source': SpellAttribute(ru_name='источник', ru_value='na',
+                                                          en_name='source', en_value='na'),
                                  'higher_levels': SpellAttribute(ru_name='на больших уровнях', ru_value='',
                                                                  en_name='higher levels', en_value='na'),
-                                 'name'         : SpellAttribute(ru_name='имя', ru_value=eng_spell_name,
-                                                                 en_name='name', en_value=''),
-                                 'description'  : SpellAttribute(ru_name='описание', ru_value='Нет описания',
-                                                                 en_name='description', en_value='No description')}
+                                 'name': SpellAttribute(ru_name='имя', ru_value=eng_spell_name,
+                                                        en_name='name', en_value=''),
+                                 'description': SpellAttribute(ru_name='описание', ru_value='Нет описания',
+                                                               en_name='description', en_value='No description')}
 
         article = articles[0]  # type: BeautifulSoup.element.Tag
         name_tag = article.find('a', attrs={'class': 'item-link', 'itemprop': 'url'})
@@ -144,10 +155,9 @@ async def fetch_spell(*, eng_spell_name: str, session: aiohttp.client.ClientSess
 
 async def open_connection_and_fetch_spells(spell_names_list: typing.List[str]):
     async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.create_task(fetch_spell(eng_spell_name=spell_name, session=session)) for spell_name in
-                 spell_names_list]
-        responses = await asyncio.gather(*tasks)
-        print(responses)
+        responses = await asyncio.gather(*[asyncio.create_task(
+                fetch_spell(eng_spell_name=spell_name, session=session)) for spell_name in spell_names_list])
+        return [r for r in responses if r]
 
 
 if __name__ == '__main__':
